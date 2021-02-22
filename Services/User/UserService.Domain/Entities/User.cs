afa3eb44d3ea367b665c67ec671ce7a8beb27d73
carol_rosa <caroline.rosa.silva@gmail.com>
2021-02-22 09:06:08 -0300

    Add UserService - api, core, domain, repository - and entity User

---
+++ Services/User/UserService.Domain/Entities/User.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Domain
+{
+    public class User
+    {
+        public int IdUser { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public int Phone { get; set; }
+        public string Picture { get; set; }
+    }
+}

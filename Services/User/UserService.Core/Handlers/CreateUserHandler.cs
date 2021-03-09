ab6d0ccd955ddd8dd2dab680e09e3d71a022e5c1
carol_rosa <caroline.rosa.silva@gmail.com>
2021-03-09 08:40:55 -0300

    CreateUser handler, request & response

---
+++ Services/User/UserService.Core/Handlers/CreateUserHandler.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using UserService.Core.Responses;
+using UserService.Core.Requests;
+using UserService.Domain.Entities;
+using MediatR;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace UserService.Core.Handlers
+{
+    public class CreateUserHandler : IRequestHandler<CreateUserRequest, CreateUserResponse>
+    {
+        public CreateUserHandler() { }
+
+        public Task<CreateUserResponse> Handle(CreateUserRequest request, CancellationToken cancellationToken)
+        {
+            var user = new User
+            {
+                Name = "foolano",
+                Email = "foo@lano.com",
+                Phone = 99999999
+            };
+
+            //_service.Create(user);
+
+            var response = new CreateUserResponse
+            {
+                IdUser = user.IdUser,
+                Name = user.Name,
+                Email = user.Email,
+                Phone = user.Phone,
+                SignUpDate = DateTime.Now
+            };
+
+            return Task.FromResult(response);
+        }
+    }
+}
---
+++ Services/User/UserService.Core/Requests/CreateUserRequest.cs
@@ -0,0 +1,12 @@
+﻿using MediatR;
+using UserService.Core.Responses;
+
+namespace UserService.Core.Requests
+{
+    public class CreateUserRequest : IRequest<CreateUserResponse>
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public int Phone { get; set; }
+    }
+}

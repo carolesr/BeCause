898c625c712a94756e5abfe6ea9359b8ca65f89d
carol_rosa <caroline.rosa.silva@gmail.com>
2021-03-15 07:56:09 -0300

    include FluentValidation in .core

--- Services/User/UserService.Core/Handlers/CreateUserHandler.cs
+++ Services/User/UserService.Core/Handlers/CreateUserHandler.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using UserService.Core.Responses;
 using UserService.Core.Requests;
+using UserService.Core.Validations;
 using UserService.Domain.Entities;
 using MediatR;
 using System.Threading.Tasks;
@@ -12,22 +13,32 @@ namespace UserService.Core.Handlers
     public class CreateUserHandler : IRequestHandler<CreateUserRequest, CreateUserResponse>
     {
         IMapper _mapper;
+        UserValidator _validator;
 
-        public CreateUserHandler(IMapper mapper) 
+        public CreateUserHandler(IMapper mapper, UserValidator validator) 
         {
             _mapper = mapper;
+            _validator = validator;
         }
 
         public Task<CreateUserResponse> Handle(CreateUserRequest request, CancellationToken cancellationToken)
         {
             var user = _mapper.Map<User>(request);
+            var validation = _validator.Validate(user);
+
+            if (!validation.IsValid)
+            {
+                foreach (var failure in validation.Errors)
+                    Console.WriteLine("Property " + failure.PropertyName + " failed validation. Error was: " + failure.ErrorMessage);
+                //return Exception;
+            }
 
             //user = _service.Create(user);
 
             var response = _mapper.Map<CreateUserResponse>(user);
             response.SignUpDate = DateTime.Now;
 
-            return Task.FromResult(response);
+            return Task.FromResult(response);            
         }
     }
 }
---
+++ Services/User/UserService.Core/Validations/UserValidator.cs
@@ -0,0 +1,15 @@
+﻿using FluentValidation;
+using UserService.Domain.Entities;
+
+namespace UserService.Core.Validations
+{
+    public class UserValidator : AbstractValidator<User>
+    {
+        public UserValidator()
+        {
+            RuleFor(u => u.Name).NotNull().NotEmpty();
+            RuleFor(u => u.Email).NotNull().NotEmpty();
+            RuleFor(u => u.Phone).NotNull().NotEmpty();
+        }
+    }
+}
